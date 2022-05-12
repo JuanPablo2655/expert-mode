7df201d9e22ed3a00baab144610e8974d5040ec2
Isidro Casiano <personal@isidrocasiano.com>
2022-05-11 23:17:56 -0400

    feat: init

--- src/main/java/net/fabricmc/expertmode/mixin/ExpertMixin.java
+++ src/main/java/net/fabricmc/expertmode/mixin/ExpertMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package net.fabricmc.expertmode.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import net.fabricmc.expertmode.Main;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class ExpertMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Main.LOGGER.info("This line is printed by an example mod mixin!");
 	}
 }
