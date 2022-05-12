13ae1e7b863e8282200f861ab408c02e65bb3d45
Isidro Casiano <personal@isidrocasiano.com>
2022-05-12 00:54:46 -0400

    feat(Logger): correct logger name

--- src/main/java/net/fabricmc/expertmode/Main.java
+++ src/main/java/net/fabricmc/expertmode/Main.java
@@ -1,6 +1,7 @@
 package net.fabricmc.expertmode;
 
 import net.fabricmc.api.ModInitializer;
+import net.minecraft.server.PlayerManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +9,7 @@ public class Main implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("expert");
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +17,6 @@ public class Main implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
+		LOGGER.info("I have no idea what I'm doing!");
 	}
 }
